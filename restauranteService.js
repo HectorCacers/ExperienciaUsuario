c39d890daae2dbf1b4380985a7f9315e2c72f5b4
HectorCacers <hectorcacers11@gmail.com>
2024-12-11 10:50:26 -0600

    service restaurant

---
+++ restauranteService.js
@@ -0,0 +1,30 @@
+const { Restaurant, RestaurantAvailability } = require('./restauranteModel');
+
+const getRestaurants = async (limit, offset) => {
+    return await Restaurant.findAll({ limit, offset });
+};
+
+const getAvailability = async () => {
+    const now = new Date();
+    return await RestaurantAvailability.findAll({
+        where: {
+            date_time: { [Sequelize.Op.gt]: now },
+            reserved: false
+        }
+    });
+};
+
+const reserveRestaurant = async (personName, scheduleTime, restaurantId) => {
+    const availability = await RestaurantAvailability.findOne({
+        where: { restaurant_id: restaurantId, date_time: scheduleTime }
+    });
+    if (!availability) throw new Error('No availability found for the given time');
+    if (availability.reserved) throw new Error('The selected time is already reserved');
+
+    availability.reserved = true;
+    availability.reserved_by = personName;
+    await availability.save();
+    return { message: 'Reservation successful' };
+};
+
+module.exports = { getRestaurants, getAvailability, reserveRestaurant };
